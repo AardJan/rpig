57052b18cb960ab726e1571347828929e4ff58fc
AardJan <jany.adrian@gmail.com>
2020-12-29 10:01:25 +0000

    Add test pump to main.py, test value from settings.ini

--- main.py
+++ main.py
@@ -2,12 +2,23 @@ from configparser import ConfigParser
 import time
 import adafruit_dht
 import board
+import RPi.GPIO as GPIO
 
 config = ConfigParser()
-config.read("setting.ini")
+config.read("settings.ini")
 PIN_TEMP = config.get("pins", "pin_thermometer")
-PIN_WATER_PUMP = config.get("pins", "pin_water_pump")
+PIN_WATER_PUMP = config.getint("pins", "pin_water_pump")
 PIN_SOIL_MOISTURE_SENSOR = config.get("pins", "pin_mcp_soil_moisture_sensor")
-print(PIN_TEMP)
+WATER_TIME = config.getint("settings", "water_time")
 print(PIN_WATER_PUMP)
+print(PIN_TEMP)
 print(PIN_SOIL_MOISTURE_SENSOR)
+print("Test PUMP")
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PIN_WATER_PUMP, GPIO.OUT, initial=1)
+
+GPIO.output(PIN_WATER_PUMP, 0)
+time.sleep(WATER_TIME)
+GPIO.output(PIN_WATER_PUMP, 1)
+print("End test")
