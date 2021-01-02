550ce4c22277eb0d7966f7519a5aeb26bd452c3f
AardJan <jany.adrian@gmail.com>
2021-01-02 21:22:02 +0100

    add render template and index.html

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+import get_info_data as gid
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World!"
+    sm = gid.get_soil_moisture()
+    temperature, humidity = gid.get_temp_and_humidity()
+    return render_template('index.html',
+                            sm = sm,
+                            temperature = temperature,
+                            humidity = humidity )
+
+@app.route("/temp")
+def temp():
+    return render_template()
     
 if __name__ == "__main__":
     app.run(host='192.168.0.101', port=5001)
